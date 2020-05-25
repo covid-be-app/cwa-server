118758e588794c1663a7fa4b281092e946812a3c
Christian Kirschnick <christian.kirschnick@sap.com>
2020-05-25 10:00:34 +0200

    update playground with submission payload

--- tools/proto-playground/src/main/java/app/coronawarn/server/tools/protoplayground/PlayGround.java
+++ tools/proto-playground/src/main/java/app/coronawarn/server/tools/protoplayground/PlayGround.java
@@ -1,6 +1,8 @@
 package app.coronawarn.server.tools.protoplayground;
 
 import app.coronawarn.server.tools.protoplayground.gen.File;
+import app.coronawarn.server.tools.protoplayground.gen.SubmissionPayload;
+import com.google.protobuf.GeneratedMessageV3;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,8 +13,10 @@ public class PlayGround {
 
   public static void main(String[] args) throws IOException {
     var loadedFile = readFile();
+    var submissionFile = readFileSubmission();
 
     loadedFile.writeTo(new FileOutputStream("result.bin"));
+    submissionFile.writeTo(new FileOutputStream("result_submission.bin"));
   }
 
   private static File readFile() {
@@ -23,6 +27,21 @@ public class PlayGround {
     InputStream inputStream = PlayGround.class.getClassLoader().getResourceAsStream(path);
 
     var loaded = yaml.loadAs(inputStream, File.newBuilder().getClass());
+    if (loaded == null) {
+      throw new RuntimeException("Unable to load - null");
+    }
+
+    return loaded.build();
+  }
+
+  private static GeneratedMessageV3 readFileSubmission() {
+    String path = "submission.yaml";
+    Yaml yaml = new Yaml(new YamlConstructorForProtoBuf());
+    yaml.setBeanAccess(BeanAccess.FIELD); /* no setters on RiskScoreParameters available */
+
+    InputStream inputStream = PlayGround.class.getClassLoader().getResourceAsStream(path);
+
+    var loaded = yaml.loadAs(inputStream, SubmissionPayload.newBuilder().getClass());
     if (loaded == null) {
       throw new RuntimeException("Unable to load - null");
     }
