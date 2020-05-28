3a90fbe5bc6e276545c77c59a0f11133d01f64cd
Pit Humke <pit.humke@sap.com>
2020-05-28 20:37:49 +0200

    Add utility test to check signatures in export files

--- services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/structure/archive/decorator/signing/SigningDecoratorTest.java
+++ services/distribution/src/test/java/app/coronawarn/server/services/distribution/assembly/structure/archive/decorator/signing/SigningDecoratorTest.java
@@ -38,6 +38,7 @@ import app.coronawarn.server.services.distribution.config.DistributionServiceCon
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -149,6 +150,33 @@ class SigningDecoratorTest {
       payloadSignature.update(fileBytes);
       assertThat(payloadSignature.verify(signatureBytes)).isTrue();
     }
+  }
+
+
+  @Test
+  void checkSignatureInFile()
+      throws IOException, CertificateException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    Resource certResource = resourceLoader.getResource("classpath:keys/certificate.crt");
+    InputStream certStream = certResource.getInputStream();
+    byte[] bytes = certStream.readAllBytes();
+    CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+    InputStream certificateByteStream = new ByteArrayInputStream(bytes);
+    Certificate certificate = certificateFactory.generateCertificate(certificateByteStream);
+
+    Resource exportResource = resourceLoader.getResource("classpath:files/export.bin");
+    Resource signatureResource = resourceLoader.getResource("classpath:files/export.sig");
+
+    byte[] exportFileBytes = Files.readAllBytes(exportResource.getFile().toPath());
+    byte[] signatureFileBytes = Files.readAllBytes(signatureResource.getFile().toPath());
+
+    TEKSignatureList tekSignatureList = TEKSignatureList.parseFrom(signatureFileBytes);
+    TEKSignature tekSignature = tekSignatureList.getSignatures(0);
+    byte[] signatureBytes = tekSignature.getSignature().toByteArray();
+
+    Signature payloadSignature = Signature.getInstance("SHA256withECDSA", "BC");
+    payloadSignature.initVerify(certificate);
+    payloadSignature.update(exportFileBytes);
+    assertThat(payloadSignature.verify(signatureBytes)).isTrue();
   }
 
   private static class TestSigningDecorator extends SigningDecoratorOnDisk {
