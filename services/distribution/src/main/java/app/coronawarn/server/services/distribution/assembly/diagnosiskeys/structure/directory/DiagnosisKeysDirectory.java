12246567f39e6b7527d696141bfa83fe022fe870
Davy De Waele <ddewaele@gmail.com>
2020-09-17 05:27:18 +0200

    CBA-411: open source prep

--- services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/directory/DiagnosisKeysDirectory.java
+++ services/distribution/src/main/java/app/coronawarn/server/services/distribution/assembly/diagnosiskeys/structure/directory/DiagnosisKeysDirectory.java
@@ -3,6 +3,7 @@
  * Corona-Warn-App
  * ---
  * Copyright (C) 2020 SAP SE and all other contributors
+ * All modifications are copyright (c) 2020 Devside SRL.
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
